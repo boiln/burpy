566d01f1c52d9599e91fb30cc62d3a95bc6a44d4
Boiln <106992326+Boiln@users.noreply.github.com>
2024-11-16 07:39:46 -0600

    cleanup tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
     darkMode: ["class"],
@@ -62,11 +61,7 @@ const config: Config = {
                 card: {
                     DEFAULT: "hsl(var(--card))",
                     foreground: "hsl(var(--card-foreground))",
-                },
-                "dark-background": "hsl(222.2 47.4% 11.2%)",
-                "dark-foreground": "hsl(210 40% 98%)",
-                "dark-muted": "hsl(217.2 32.6% 17.5%)",
-                "dark-muted-foreground": "hsl(215 20.2% 65.1%)",
+                }
             },
             borderRadius: {
                 lg: "var(--radius)",
@@ -90,50 +85,11 @@ const config: Config = {
                         height: "0",
                     },
                 },
-                "fade-in": {
-                    "0%": {
-                        opacity: "0",
-                        transform: "translateY(2px)",
-                    },
-                    "100%": {
-                        opacity: "1",
-                        transform: "translateY(0)",
-                    },
-                },
-                "fade-in-25": {
-                    "0%": {
-                        opacity: "0",
-                    },
-                    "100%": {
-                        opacity: "1",
-                    },
-                },
-                enter: {
-                    "0%": {
-                        opacity: "0",
-                    },
-                    "100%": {
-                        opacity: "1",
-                    },
-                },
-                exit: {
-                    to: {
-                        opacity: "var(--tw-exit-opacity, 1)",
-                        transform:
-                            "translate3d(var(--tw-exit-translate-x, 0), var(--tw-exit-translate-y, 0), 0) scale3d(var(--tw-exit-scale, 1), var(--tw-exit-scale, 1), var(--tw-exit-scale, 1)) rotate(var(--tw-exit-rotate, 0))",
-                    },
-                },
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
-                "fade-in": "enter 200ms ease-out",
-                in: "enter 0.15s ease-out",
-                out: "exit 0.15s ease-out",
             },
-        },
-        screens: {
-            xs: "480px",
         },
     },
     plugins: [require("tailwindcss-animate")],
