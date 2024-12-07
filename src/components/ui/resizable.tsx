67f25646c9c20965119b2526a3f7afbfd44455b5
Boiln <106992326+Boiln@users.noreply.github.com>
2024-12-07 06:34:11 -0600

    reduce resize handle

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -24,9 +24,9 @@ const ResizableHandle = ({
 }: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle>) => (
     <ResizablePrimitive.PanelResizeHandle
         className={cn(
-            "group relative flex items-center justify-center",
-            "w-4 bg-transparent hover:cursor-col-resize",
-            "data-[panel-group-direction=vertical]:h-4 data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:hover:cursor-row-resize",
+            "group relative mx-1 flex items-center justify-center",
+            "w-1 bg-transparent hover:cursor-col-resize",
+            "data-[panel-group-direction=vertical]:h-1 data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:hover:cursor-row-resize",
             "before:absolute before:flex before:transition-[background-color] before:duration-0",
             "before:h-full before:w-[2px] before:rounded-full before:bg-border group-hover:before:bg-primary/60",
             "data-[panel-group-direction=vertical]:before:h-[2px] data-[panel-group-direction=vertical]:before:w-full",
