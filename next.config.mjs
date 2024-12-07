b4623c0a65daa713d9a2f4f2e9e5d240817d384c
Boiln <106992326+Boiln@users.noreply.github.com>
2024-12-07 06:52:56 -0600

    add github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
+    basePath: "/burpy",
+    images: {
+        unoptimized: true,
+    },
     webpack: (config) => {
         config.experiments = {
             ...config.experiments,
